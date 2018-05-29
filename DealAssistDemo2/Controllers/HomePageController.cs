af52ca65a6f022ba53c608d449c38b0c68e27799
long8 <long8@DESKTOP-J6UT3SD>
2018-05-29 11:21:20 +0700

    add sourcce

--- DealAssistDemo2/Controllers/HomePageController.cs
+++ DealAssistDemo2/Controllers/HomePageController.cs
@@ -10,10 +10,17 @@ namespace DealAssistDemo2.Controllers
     public class HomePageController : Controller
     {
         public ListHome model = new ListHome();
+        public int numberofdata = 5;
+        /* get number of packages from SQL */
         // GET: HomePage
         public ActionResult Index()
         {
             var data = model.sanpham;
+            for(int i = 0; i < numberofdata; i++)
+            {
+                string sanpham = "Laptop no" + (i + 1).ToString();
+                data.Add(sanpham);
+            }
             ViewBag.urlimgsanpham = model.urlimage;
             ViewBag.sanpham = data;
             return View();
