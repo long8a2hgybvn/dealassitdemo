d86eb0d64cc8f91c926f1ca0c431276031027965
long8 <long8@DESKTOP-J6UT3SD>
2018-06-05 17:05:08 +0700

    cataview

--- DealAssistDemo2/Models/CatalogueView.cs
+++ DealAssistDemo2/Models/CatalogueView.cs
@@ -7,6 +7,20 @@ namespace DealAssistDemo2.Models
 {
     public class CatalogueView
     {
-        
+        public List<string> docongnghe = new List<string>()
+        {
+            "Điện thoại di động",
+            "Máy tính bảng",
+            "Laptop",
+            "Máy tính bàn",
+            "Loa và Tai nghe",
+            "Máy chơi game",
+            "Camera",
+            "Phụ kiện di động",
+            "Phụ kiện máy tính",
+            "Thiết bị mạng",
+            "Thiết bị đeo",
+            "Thiết bị lưu trữ"
+        };
     }
 }
